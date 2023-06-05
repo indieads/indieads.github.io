546a08584e66bdd91bd61f395d5663e5f5e16355
a1-creator <asbjorn15johnsen@gmail.com>
2023-06-05 16:02:22 +0200

    spawnIndieAd(parent, res) function added

--- indieads.js
+++ indieads.js
@@ -13,27 +13,22 @@ var AdResolutions =
 }
 
 
-function spawnAds()
-{
-    var adContainers = document.body.getElementsByClassName("indieads");
 
-    for (let i = 0; i < adContainers.length; i++) {
-        
-        var linkElement = document.createElement("a");
-        linkElement.target = "_blank";
-        linkElement.className = "indiead-link"
-        linkElement.id = `indieads-link-${i}`;
-        
-        var imgElement = document.createElement("img");
-        imgElement.className = "indiead-img"
-        imgElement.id = `indieads-img-${i}`;
-        
-        linkElement.append(imgElement);
-        adContainers[i].append(linkElement);
-        
-        
-        fetchIndieAd(linkElement.id, imgElement.id, AdResolutions.Portrait_600x900)
-    }
+function spawnIndieAd(container, resolution)
+{
+    var linkElement = document.createElement("a");
+    linkElement.target = "_blank";
+    linkElement.className = "indiead-link"
+    linkElement.id = `indieads-link-${i}`;
+    
+    var imgElement = document.createElement("img");
+    imgElement.className = "indiead-img"
+    imgElement.id = `indieads-img-${i}`;
+    
+    linkElement.append(imgElement);
+    container.append(linkElement);
+    
+    fetchIndieAd(linkElement.id, imgElement.id, resolution)
 }
 
 async function fetchIndieAd(link_elementID, img_elementID, resolution) {
